c226dc5d4b2014b10fcca0b3b7c964268fdd7a3e
Bill Davidsen <Bill@Xensan.com>
2020-12-18 15:05:03 -0500

    Added BloC pattern to login screen

---
+++ lib/screens/login/login_form.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:homephotos_app/bloc/bloc-prov.dart';
+
+import 'login_bloc.dart';
+
+class LoginForm extends StatefulWidget {
+
+  @override
+  LoginFormState createState() {
+    return LoginFormState();
+  }
+}
+
+class LoginFormState extends State<LoginForm> {
+  LoginBloc _bloc;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _bloc = BlocProvider.of(context);
+  }
+
+  @override
+  void dispose() {
+    _bloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        children: <Widget>[
+          usernameField(),
+          SizedBox(height: 8.0),
+          passwordField(),
+          SizedBox(height: 24.0),
+          loginButton(),
+          SizedBox(height: 8.0),
+          registerLabel()
+        ]
+    );
+  }
+
+  Widget usernameField() {
+    return StreamBuilder(
+        stream: _bloc.username,
+        builder: (context, snapshot) {
+          return TextField(
+            onChanged: _bloc.changeUsername,
+            decoration: InputDecoration(
+                hintText: 'Username',
+                errorText: snapshot.error,
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32.0)
+                ),
+            ),
+          );
+        });
+  }
+
+  Widget passwordField() {
+    return StreamBuilder(
+        stream: _bloc.password,
+        builder: (context, AsyncSnapshot<String> snapshot) {
+          return TextField(
+            onChanged: _bloc.changePassword,
+            autofocus: false,
+            obscureText: true,
+            decoration: InputDecoration(
+                hintText: 'Password',
+                errorText: snapshot.error,
+                contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32.0)
+                ),
+            ),
+          );
+        });
+  }
+
+  Widget loginButton() {
+    return StreamBuilder(
+        stream: _bloc.signInStatus,
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          // if (!snapshot.hasData || snapshot.hasError) {
+          //
+          // }
+          return button();
+        });
+  }
+
+  Widget button() {
+    return Padding(
+        padding: EdgeInsets.symmetric(vertical: 16.0),
+        child: Material(
+            borderRadius: BorderRadius.circular(30.0),
+            shadowColor: Colors.lightBlueAccent.shade100,
+            elevation: 5.0,
+            color: Colors.lightBlueAccent,
+            child: MaterialButton(
+              minWidth: 200.0,
+              height: 42.0,
+              onPressed: () {
+                if (_bloc.validateFields()) {
+                  authenticateUser();
+                }
+                else {
+                  showErrorMessage('Invalid username/password');
+                }
+              },
+              child: Text('Sign-In',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.0
+                ),
+              ),
+            )
+        )
+    );
+  }
+
+  Widget registerLabel() {
+    return FlatButton(
+        onPressed: () {},
+        child: Text(
+          'Register a new account',
+          style: TextStyle(color: Colors.black54),
+        )
+    );
+  }
+
+  void authenticateUser() {
+    _bloc.showProgressBar(true);
+    _bloc.submit().then((success) {
+      if (success) {
+        Navigator.of(context).pushNamed('/photos');
+      }
+      else {
+        showErrorMessage('Login failed');
+      }
+    });
+  }
+
+  void showErrorMessage(String message) {
+    final snackbar = SnackBar(
+        content: Text(message),
+        duration: new Duration(seconds: 2)
+    );
+    Scaffold.of(context).showSnackBar(snackbar);
+  }
+}
--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,133 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:homephotos_app/bloc/bloc-prov.dart';
-import 'package:homephotos_app/blocs/auth-bloc.dart';
+import 'login_bloc.dart';
+import 'login_form.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   LoginScreen({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  AuthBloc authBloc;
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    authBloc = BlocProvider.of(context);
+  Widget build(BuildContext context) {
+    return BlocProvider<LoginBloc>(
+        bloc: LoginBloc(),
+        child: Container(
+          child: Scaffold(
+              backgroundColor: Colors.white,
+              body: Center(
+                  child: ListView(
+                    shrinkWrap: true,
+                    padding: EdgeInsets.only(
+                      left: 24.0,
+                      right: 24.0,
+                    ),
+                    children: <Widget>[
+                      SizedBox(height: 48.0),
+                      logo,
+                      nameLabel,
+                      SizedBox(height: 48.0),
+                      LoginForm()
+                    ],
+                  )
+              )
+          ),
+        )
+    );
   }
 
-  @override
-  Widget build(BuildContext context) {
-
-    final logo = Hero(
-        tag: 'hero', 
-        child: CircleAvatar(
+  final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
           backgroundColor: Colors.transparent,
           radius: 48.0,
           child: Image.asset('assets/img/logo-lg.png')
-        )
-    );
-
-    final nameLabel = FlatButton(
-        onPressed: () {},
-        child: Text(
-          'ePhotoBox',
-          style: TextStyle(
-              color: Colors.black87,
-              fontSize: 20.0
-          ),
-        )
-    );
-
-    final username = TextFormField(
-      keyboardType: TextInputType.text,
-      autofocus: true,
-      decoration: InputDecoration(
-        hintText: 'Username',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(32.0)
-        ),
-      ),
-    );
-
-    final password = TextFormField(
-      keyboardType: TextInputType.text,
-      autofocus: false,
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: 'Password',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(32.0)
-        ),
-      ),
-    );
-    
-    final loginButton = Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: Material(
-        borderRadius: BorderRadius.circular(30.0),
-        shadowColor: Colors.lightBlueAccent.shade100,
-        elevation: 5.0,
-        color: Colors.lightBlueAccent,
-        child: MaterialButton(
-          minWidth: 200.0,
-          height: 42.0,
-          onPressed: () async {
-            if (await authBloc.login('wdavidsen', 'Pass@123')) {
-              Navigator.of(context).pushNamed('/photos');
-            }
-            else {
-
-            }
-          },
-          child: Text('Sign-In',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18.0
-            ),
-          ),
-        )
       )
-    );
+  );
 
-    final registerLabel = FlatButton(
+  final nameLabel = FlatButton(
       onPressed: () {},
       child: Text(
-        'Register a new account',
-        style: TextStyle(color: Colors.black54),
+        'ePhotoBox',
+        style: TextStyle(
+            color: Colors.black87,
+            fontSize: 20.0
+        ),
       )
-    );
-
-    return Scaffold(
-        backgroundColor: Colors.white,
-        body: Center(
-            child: ListView(
-              shrinkWrap: true,
-              padding: EdgeInsets.only(
-                left: 24.0,
-                right: 24.0,
-              ),
-              children: <Widget>[
-                SizedBox(height: 48.0),
-                logo,
-                nameLabel,
-                SizedBox(height: 48.0),
-                username,
-                SizedBox(height: 8.0),
-                password,
-                SizedBox(height: 24.0),
-                loginButton,
-                SizedBox(height: 8.0),
-                registerLabel
-              ],
-            )
-        )
-    );
-  }
+  );
 }
